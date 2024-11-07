569a0429ba3ec472c524d4523ff0055257e641a4
Woods <162640861+TatoGar12@users.noreply.github.com>
2024-11-07 13:12:27 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,1 +1,1 @@
-https://prod.liveshare.vsengsaas.visualstudio.com/join?86B28E55EA3E53849729E223D2CA44E7B6CF
+https://prod.liveshare.vsengsaas.visualstudio.com/join?A8C94F60FA07FA0447BA6A79EAC46D6D4162
