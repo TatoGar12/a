42bf134c86d3477cec84b1022df1f8cdd70d7baa
Woods <162640861+TatoGar12@users.noreply.github.com>
2024-11-07 12:57:33 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-
-# Actualizar paquetes del sistema
-sudo apt-get update -y
-
-# Instalar Docker Compose
-DOCKER_COMPOSE_VERSION="2.0.0"
-sudo curl -L "https://github.com/docker/compose/releases/download/v$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-# Aplicar permisos ejecutables a Docker Compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-# Verificar la instalación de Docker Compose
-docker-compose --version
-
+https://prod.liveshare.vsengsaas.visualstudio.com/join?86B28E55EA3E53849729E223D2CA44E7B6CF
